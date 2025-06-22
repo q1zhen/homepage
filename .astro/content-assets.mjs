42ea51005f818537f924494115307edffa20fab4
Qizhen Yang <142765265+q1zhen@users.noreply.github.com>
2025-06-22 14:41:26 +0800

    been here

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_1hDuqo from "../src/assets/windows-features.png?astroContentImageFlag=&importer=blog%2Fenhancing-latex-compiling.md";
-export default new Map([["../src/assets/windows-features.png?astroContentImageFlag=&importer=blog%2Fenhancing-latex-compiling.md", __ASTRO_IMAGE_IMPORT_1hDuqo]]);
-		
+export default new Map();
--- src/components/nav-items.md
+++
@@ -1,7 +0,0 @@
-- [Home](/)
-- [Blog](/blog)
-- [Pro&shy;jects&#8203;&&#8203;Tools](/projects)
-- [About](/about)
-
-+ [GitHub](https://github.com/q1zhen)
-+ [bsky](https://bsky.app/profile/q1z.dev)
